e34f407e14c7a757605fba2a6aa7991851efbe2c
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-03 14:33:54 +0400

    Creating a new controller class

--- demoX/src/main/java/com/example/demoX/BookstoreController.java
+++ demoX/src/main/java/com/example/demoX/BookstoreController.java
@@ -1,7 +1,9 @@
 package com.example.demoX;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,11 @@ import java.util.List;
 public class BookstoreController {
 
     static List<InventoryBook> inventoryBooks = new ArrayList<>();
+
+    @GetMapping("/addInventoryBook")
+
+
+            //Integer id, String title, Integer stockCount, Double price);
 
 
 
