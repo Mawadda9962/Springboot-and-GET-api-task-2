22abb7162c530a78a4982292e50f779b62690dd8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-03 14:22:07 +0400

    Adding Constructor

--- demoX/src/main/java/com/example/demoX/InventoryBook.java
+++ demoX/src/main/java/com/example/demoX/InventoryBook.java
@@ -6,6 +6,10 @@ public class InventoryBook {
     private Double price;
     private Integer StockCount;
 
-
-
+    public InventoryBook(Integer bookId, String title, Integer stockCount, Double price) {
+        BookId = bookId;
+        this.title = title;
+        StockCount = stockCount;
+        this.price = price;
+    }
 }
