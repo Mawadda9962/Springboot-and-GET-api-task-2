a90314e73664df6cdfa614cfe1441bc98d6ef013
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-03 11:25:22 +0400

    Addin Constructor

--- demoX/src/main/java/com/example/demoX/Author.java
+++ demoX/src/main/java/com/example/demoX/Author.java
@@ -11,6 +11,28 @@ public class Author {
         this.id = id;
         this.biograph = biograph;
     }
-    \
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiograph() {
+        return biograph;
+    }
+
+    public void setBiograph(String biograph) {
+        this.biograph = biograph;
+    }
 }
