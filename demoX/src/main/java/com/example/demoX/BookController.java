6759da809fbac49af3aca8c07089087fb3c8c11b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-03 09:37:54 +0400

    Creating a new java class

--- demoX/src/main/java/com/example/demoX/BookController.java
+++ demoX/src/main/java/com/example/demoX/BookController.java
@@ -1,4 +1,12 @@
 package com.example.demoX;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class BookController {
+
+    private static List<Book> bookshelf = new ArrayList<Book>();
 }
