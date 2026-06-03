3e126a8ef11a40d2905e9bee527138b8da5f3f95
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-03 11:33:54 +0400

    Creating a new controller

--- demoX/src/main/java/com/example/demoX/LibraryController.java
+++ demoX/src/main/java/com/example/demoX/LibraryController.java
@@ -2,7 +2,11 @@ package com.example.demoX;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class LibraryController {
+    private static List<Author> authorList;
+
 
 }
