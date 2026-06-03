3b83ad610a34a7a915aaa1b2f6848080ef0ae3c9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-03 11:29:16 +0400

    Updating the constructor and getter and setter on the book class

--- demoX/src/main/java/com/example/demoX/Book.java
+++ demoX/src/main/java/com/example/demoX/Book.java
@@ -4,25 +4,10 @@ public class Book {
 
     private int id;
     private String name;
-    String authorId;
+    private String authorId;
 
-    public Book(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+
 }
